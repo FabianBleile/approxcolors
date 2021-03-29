dbfb29e5b3d7457eb4a9308afa242ff3b1bb1a65
Fabian <fabian.bleile@web.de>
2021-03-29 11:08:57 +0200

    dsatur done, vector shuffle needs to be done better in dsatur() & greedy

--- mmt.cpp
+++ mmt.cpp
@@ -8,6 +8,7 @@ extern "C" {
 #include <unordered_map>
 #include <algorithm>
 #include <numeric>
+#include <utility>
 
 class MMTGraph {
 public:
@@ -69,10 +70,9 @@ public:
   MMTPartialColoring(const int k, const MMTGraph * igraph) : k(k), colors(), graph(igraph) {
     assert(k!=0);
     assert(igraph != NULL);
-    // generate random seq of nodes
+    // generate ascending seq of nodes
     std::vector<int> rand_vect(graph->n);
     std::iota(rand_vect.begin(), rand_vect.end(), 0);
-    std::random_shuffle(rand_vect.begin(), rand_vect.end());
     // insert nodes in (k+1)-st bucket
     for (const auto &u : rand_vect) this->colors.emplace(u, k);
 
@@ -81,33 +81,52 @@ public:
   void greedy() {
     // SEQ
     // randomized insertion might not affect hashing in unordered_map ... idk(?)
-    for (auto &kvp : this->colors) kvp.second = findMinAvailableColor(kvp.first);
+    // generate random seq of nodes
+    std::vector<int> rand_vect(graph->n);
+    std::iota(rand_vect.begin(), rand_vect.end(), 0);
+    std::random_shuffle(rand_vect.begin(), rand_vect.end());
+    for (const auto &u : rand_vect) this->colors[u] = findMinAvailableColor(u);
   }
   // clear (k+1)-st bucket in a dsatur way
   void dsatur(){
     // compute initial degrees in G
-    std::vector<int> v_dsatur;
-    dsatur_selectMaxNodes([] (const MMTGraph* context, int u) -> int { return static_cast<const MMTGraph*>(context)->getDegree(u); }, v_dsatur, this->graph);
-    std::vector<int> deg_dsatur(this->graph->n, 0);
+    std::vector<std::pair<int, int> > deg_dsatur(this->graph->n); // degU, degDsatur
+    int u = 0;
+    std::generate(deg_dsatur.begin(), deg_dsatur.end(), [&] () mutable { return std::make_pair(this->graph->getDegree(u++), 0); });
+    int max_node;
     for (size_t i = 0; i < this->graph->n; i++) {
-      // compute max degree
-      for (size_t i = 0; i < this->graph->n; i++) {
-        /* code */
-      }
+      // compute random node with max degree in G from all nodes with max saturation
+      max_node = dsatur_selectMaxNode(deg_dsatur);
+      // color max_node with the lowest available color
+      this->colors[max_node] = findMinAvailableColor(max_node);
+      // update degrees in G and C
+      dsatur_updateSatDeg(max_node, deg_dsatur);
     }
   }
 
-  void dsatur_selectMaxNodes(std::function<int(const MMTGraph*, const int&)> getDegree, std::vector<int>& v, const MMTGraph* context = null) const {
-    v.clear();
-    v = {0};
+  int dsatur_selectMaxNode(std::vector<std::pair<int, int> >& degs) const {
+    std::vector<int> v = {0};
     for (size_t i = 1; i < this->graph->n; i++) {
-      if(getDegree(context, i) > getDegree(context, v[0])) {
+      if(degs[v[0]].second < degs[i].second || (degs[v[0]].second == degs[i].second && degs[v[0]].first < degs[i].first)) {
         v.clear();
         v.push_back(i);
-      } else if (getDegree(context, i) == getDegree(context, v[0])) {
+      } else if (degs[v[0]].second == degs[i].second && degs[v[0]].first == degs[i].first) {
         v.push_back(i);
       }
     }
+    std::random_shuffle(v.begin(), v.end());
+    return v[0];
+  }
+
+  void dsatur_updateSatDeg(int u, std::vector<std::pair<int, int> >& degs){
+    const std::unordered_set<int> * u_neighbors = this->graph->getNeighbors(u);
+    for (const auto &v : *u_neighbors) {
+      if (degs[v].second != -1) {
+        degs[v].first--; // remove from G (uncolored Graph)
+        degs[v].second++; // add to C (colored Graph)
+      }
+    }
+    degs[u].second = -1;
   }
 
   int findMinAvailableColor(int u) {
@@ -167,7 +186,8 @@ int main(int argc, char **av) {
 
   MMTPartialColoring pc(5, &g);
 
-  pc.dsatur();
+  pc.greedy();
+  pc.toString();
 
   return 0;
 }
