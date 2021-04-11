174fa57e73b2e43208eec3956813ed936072e88e
Fabian <fabian.bleile@web.de>
2021-04-11 17:40:09 +0200

    slight adjustment on time limit, output text

--- mmt.cpp
+++ mmt.cpp
@@ -1,29 +1,113 @@
 #include "mmt_graph.h"
 #include "mmt_partial_coloring.h"
 
+#include <time.h>
+#include <vector>
+#include <utility>
+
+class MMT {
+public:
+  MMT(int argc, char **av, int L, int T, int time_limit_sec, int pool_size = 99) : graph(argc,av), L(L), T(T), time_limit_sec(time_limit_sec), pool_size((pool_size/3)*3), cur_best_coloring(MMTPartialColoring(graph.n, &graph, L, T)) {
+    // compute lower bound
+    // compute upper bound
+    UB = graph.n + 1;
+    LB = 9;
+  }
+
+  void testing(){
+    MMTPartialColoring A = MMTPartialColoring(7, &graph, L, T);
+    MMTPartialColoring B = MMTPartialColoring(7, &graph, L, T);
+    MMTPartialColoring C = MMTPartialColoring(7, &graph, L, T);
+
+    A.tabuSearch();
+    A.toString();
+    B.tabuSearch();
+    B.toString();
+    C.crossover(&A, &B);
+    C.lockColoring();
+    C.toString();
+  }
+
+  void EAOptimizer() {
+    clock_t t = clock();
+    while (((float) clock() - t)/CLOCKS_PER_SEC < time_limit_sec && UB > LB) {
+      EADecision(UB);
+      cur_best_coloring.toString();
+      UB--;
+    }
+    std::cout << "solution found ? " << '\t';
+    UB == LB ? std::cout << "YAAY in " << ((float) clock() - t)/CLOCKS_PER_SEC << " secs" << '\n' : std::cout << "no we timed out :o" << '\n';
+  }
+
+  void EADecision(int UB) {
+    // generate initial pool of solutions
+    std::vector<MMTPartialColoring> pool(pool_size, MMTPartialColoring(UB-1, &graph, L, T));
+    for (size_t i = 0; i < pool_size - 2; ) {
+      if(pool.at(i).greedy()) {
+        cur_best_coloring = pool[i];
+        return;
+      }
+      i++;
+      if(pool.at(i).dsatur()) {
+        cur_best_coloring = pool[i];
+        return;
+      }
+      i++;
+      if(pool.at(i).tabuSearch()) {
+        cur_best_coloring = pool[i];
+        return;
+      }
+      i++;
+    }
+
+    clock_t t = clock();
+    while (((float) clock() - t)/CLOCKS_PER_SEC < time_limit_sec) {
+      unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+      std::shuffle(pool.begin(), pool.end(), std::default_random_engine(seed));
+
+      MMTPartialColoring crossover_child(UB-1, &graph, L, T);
+      if(crossover_child.crossover(&pool[0], &pool[1]) || crossover_child.tabuSearch()) {
+        cur_best_coloring = crossover_child;
+        return;
+      }
+
+      /*
+
+        priority greedy needs to be called here
+        not implemented yet
+
+      */
+      std::cout << crossover_child.evaluate() << '\n';
+      // for (const auto & u : crossover_child.uncolored) {
+      //   std::cout << u << '\t';
+      // }
+      // std::cout << '\n';
+      // delete worst parent and insert child to pool
+      if (pool[0].evaluate() <= pool[1].evaluate()) {
+        pool[1] = crossover_child;
+      } else {
+        pool[0] = crossover_child;
+      }
+    }
+  }
+
+  MMTPartialColoring* getColoring(){
+    return &cur_best_coloring;
+  }
+
+private:
+  int L, T, time_limit_sec, pool_size;
+  MMTGraph graph;
+  int UB, LB;
+  MMTPartialColoring cur_best_coloring;
+};
+
 int main(int argc, char **av) {
 
-  MMTGraph g(argc, av);
+  MMT mmt(argc, av, /*L*/ 1000,/*T*/ 100, /*time limit*/ 60, /*pool size*/ 99);
 
-  g.toString();
-
-  int GOAL = 6;
-
-  MMTPartialColoring tbs(GOAL, &g, 3000, 100);
-  tbs.tabuSearch();
-  tbs.toString(GOAL+1);
-
-  // MMTPartialColoring seq(GOAL, &g, 3000, 100);
-  // seq.greedy();
-  // seq.toString(GOAL+1);
-
-  MMTPartialColoring dsatur(GOAL, &g, 3000, 100);
-  dsatur.dsatur();
-  dsatur.toString(GOAL+1);
-
-  MMTPartialColoring crossover(GOAL, &g, 3000, 100);
-  crossover.crossover(&tbs, &crossover);
-  crossover.toString(GOAL+1);
+  mmt.EAOptimizer();
+  // mmt.testing();
 
   return 0;
 }
